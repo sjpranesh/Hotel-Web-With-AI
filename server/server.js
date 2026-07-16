d03184a3ef02a0d4e5dffccbbcc7c3f78911f68d
Pranesh S J <sjpranesh3@gmail.com>
2026-07-16 14:52:12 +0530

    Check database name

--- server/server.js
+++ server/server.js
@@ -50,6 +50,13 @@ initSocket(io);
 // MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
+    console.log("✅ MongoDB Connected");
+    console.log("Connected database:", mongoose.connection.name);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  .then(() => {
     console.log('✅ MongoDB Connected');
     server.listen(process.env.PORT || 5000, () => {
       console.log(`🚀 Server running on port ${process.env.PORT || 5000}`);
