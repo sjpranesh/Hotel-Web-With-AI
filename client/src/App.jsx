d2d733a2c39e02bf3a5e3c7e44c7600b9aa2466e
Pranesh S J <sjpranesh3@gmail.com>
2026-07-16 15:45:37 +0530

    Added Landing Page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { HotelProvider } from './context/HotelContext';
 import { LanguageProvider } from './context/LanguageContext';
+
+import LandingPage from './pages/LandingPage';
 import MenuPage from './pages/MenuPage';
 import CartPage from './pages/CartPage';
 import PaymentPage from './pages/PaymentPage';
@@ -14,20 +17,37 @@ function App() {
   return (
     <LanguageProvider>
       <HotelProvider>
-      <Router>
-        <div className="min-h-screen text-[#1a1a1a] font-sans selection:bg-hotel-gold selection:text-white">
-          <Routes>
-            <Route path="/" element={<MenuPage />} />
-            <Route path="/menu" element={<MenuPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/payment" element={<PaymentPage />} />
-            <Route path="/success" element={<SuccessPage />} />
-            <Route path="/running-bill" element={<RunningBill />} />
-            <Route path="/kitchen" element={<KitchenDashboard />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-          </Routes>
-        </div>
-      </Router>
+        <Router>
+          <div className="min-h-screen text-[#1a1a1a] font-sans selection:bg-hotel-gold selection:text-white">
+            <Routes>
+
+              {/* Landing Page */}
+              <Route path="/" element={<LandingPage />} />
+
+              {/* Menu */}
+              <Route path="/menu" element={<MenuPage />} />
+
+              {/* Cart */}
+              <Route path="/cart" element={<CartPage />} />
+
+              {/* Payment */}
+              <Route path="/payment" element={<PaymentPage />} />
+
+              {/* Success */}
+              <Route path="/success" element={<SuccessPage />} />
+
+              {/* Running Bill */}
+              <Route path="/running-bill" element={<RunningBill />} />
+
+              {/* Kitchen Dashboard */}
+              <Route path="/kitchen" element={<KitchenDashboard />} />
+
+              {/* Admin Dashboard */}
+              <Route path="/admin" element={<AdminDashboard />} />
+
+            </Routes>
+          </div>
+        </Router>
       </HotelProvider>
     </LanguageProvider>
   );
